39b6ec14aba7198f9b99a60e801ce9d1889c8b87
D3fr0s7 <94324030+D3fr0s7@users.noreply.github.com>
2024-08-30 22:22:20 -0500

    Add files via upload

---
+++ EditCode.bat
@@ -0,0 +1,2 @@
+echo|set /p="cd %~dp0 & code . & TASKKILL /F /IM WindowsTerminal.exe"|clip
+START "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio 2022\Visual Studio Tools\VC\x86 Native Tools Command Prompt for VS 2022.lnk"
